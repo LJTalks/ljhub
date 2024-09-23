4db213a211a406662d26ec9e266a898a8deb63b2
Laura Read <laurajmtb@gmail.com>
2024-09-23 12:47:55 +0000

    Rename status choices in comment and post models to be more descriptive

--- blog/models.py
+++ blog/models.py
@@ -2,8 +2,16 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Post Status Choices
-STATUS = ((0, "Draft"), (1, "Published"))
+POST_STATUS_CHOICES = [
+    (0, "Draft"),
+    (1, "Published")
+]
 
+# Comment status choices
+COMMENT_STATUS_CHOICES = [
+    (0, 'Pending'),
+    (1, 'Approved')
+]
 
 class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)
@@ -12,7 +20,7 @@ class Post(models.Model):
         User, on_delete=models.CASCADE, related_name="blog_posts")
     content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
-    status = models.IntegerField(choices=STATUS, default=0)
+    status = models.IntegerField(choices=POST_STATUS_CHOICES, default=0)
 
     class Meta:
         ordering = ['-created_on']
@@ -20,9 +28,8 @@ class Post(models.Model):
     def __str__(self):
         return self.title
 
+
 # Adding Comment Model
-
-
 class Comment(models.Model):
     post = models.ForeignKey(
         Post, related_name='comments', on_delete=models.CASCADE)
@@ -30,11 +37,7 @@ class Comment(models.Model):
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    status = models.IntegerField(choices=STATUS_CHOICES, default=0)
-    STATUS_CHOICES = [
-        (0, 'Pending'),
-        (1, 'Approved')
-    ]
+    status = models.IntegerField(choices=COMMENT_STATUS_CHOICES, default=0)
     
 
     def __str__(self):
